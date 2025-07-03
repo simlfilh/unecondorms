e99c0b9d70940057e7731ddd75e0db1809a687b6
Valeria <148010378+simlfilh@users.noreply.github.com>
2025-07-03 16:47:27 +0300

    Update main_page.py

--- ver1/main_page.py
+++ ver1/main_page.py
@@ -13,7 +13,7 @@ st.markdown("""
     Здесь вы найдете актуальную информацию о каждом общежитии, правила проживания и контакты администрации.
 """)
 
-col1, col2, col3, col4 = st.column([1, 1, 1, 1])
+col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
 with col1:
     components.html("""
         <a href="https://chkalovski27.streamlit.app/">
@@ -71,7 +71,7 @@ with col4:
             </a>
         """)
 
-col5, col6, col7, col8 = st.column([1, 1, 1, 1])
+col5, col6, col7, col8 = st.columns([1, 1, 1, 1])
 with col5:
     st.image("здание_общежития_ч27.jpg")
 with col6:
